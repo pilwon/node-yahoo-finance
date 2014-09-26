49b0605165ba114b2a59afec8dc972a57b989c48
Pilwon Huh <pilwon@gmail.com>
2014-09-25 22:24:34 -0400

    Updated examples.

--- examples/callback/snapshot-multiple.js
+++ examples/callback/snapshot-multiple.js
@@ -5,6 +5,29 @@ require('colors');
 var _ = require('lodash');
 var yahooFinance = require('../..');
 
+var FIELDS = _.flatten([
+  // Pricing
+  ['a', 'b', 'b2', 'b3', 'p', 'o'],
+  // Dividends
+  ['y', 'd', 'r1', 'q'],
+  // Date
+  ['c1', 'c', 'c6', 'k2', 'p2', 'd1', 'd2', 't1'],
+  // Averages
+  ['c8', 'c3', 'g', 'h', 'k1', 'l', 'l1', 't8', 'm5', 'm6', 'm7', 'm8', 'm3', 'm4'],
+  // Misc
+  ['w1', 'w4', 'p1', 'm', 'm2', 'g1', 'g3', 'g4', 'g5', 'g6'],
+  // 52 Week Pricing
+  ['k', 'j', 'j5', 'k4', 'j6', 'k5', 'w'],
+  // System Info
+  ['i', 'j1', 'j3', 'f6', 'n', 'n4', 's1', 'x', 'j2'],
+  // Volume
+  ['v', 'a5', 'b6', 'k3', 'a2'],
+  // Ratio
+  ['e', 'e7', 'e8', 'e9', 'b4', 'j4', 'p5', 'p6', 'r', 'r2', 'r5', 'r6', 'r7', 's7'],
+  // Misc
+  ['t7', 't6', 'i5', 'l2', 'l3', 'v1', 'v7', 's6', 'e1']
+]);
+
 var SYMBOLS = [
   'AAPL',
   'GOOG',
@@ -29,8 +52,8 @@ var SYMBOLS = [
 ];
 
 yahooFinance.snapshot({
-  symbols: SYMBOLS,
-  fields: ['s', 'l1', 'd1', 't1', 'c1', 'o', 'h', 'g']
+  fields: FIELDS,
+  symbols: SYMBOLS
 }, function (err, result) {
   if (err) { throw err; }
   _.each(result, function (snapshot, symbol) {
